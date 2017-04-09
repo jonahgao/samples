f596a72b3d88546e81972b99e30172a72f00b806
byteink <byteink9@gmail.com>
2017-04-09 22:23:35 +0800

    cpp-template: add 5_4

---
+++ cpp-templates/chapter5_技巧性基础知识/5_4_模板的模板参数.cpp
@@ -0,0 +1,15 @@
+//
+// Created by jonah on 17-4-9.
+//
+// 在stack的例子中，如果我们要指定底层容器的类型，就必须指定两次元素类型：
+// Stack<int, std::vector<int> > vStack; // 两次int
+
+// 借助于模板的模板参数，可以只指定容器的类型而不需要指定所含元素的类型 就像这样:
+// Stack<int, std::vector> vStack;
+
+
+int main(void) {
+
+}
+
+
