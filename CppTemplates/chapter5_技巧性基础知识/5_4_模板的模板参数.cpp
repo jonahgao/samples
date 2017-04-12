5e9c2075ae6c2253eb6bb7cf5510aa564b8456b6
byteink <byteink9@gmail.com>
2017-04-12 22:31:28 +0800

    CppTempates: update 5_4

--- CppTemplates/chapter5_技巧性基础知识/5_4_模板的模板参数.cpp
+++ CppTemplates/chapter5_技巧性基础知识/5_4_模板的模板参数.cpp
@@ -6,10 +6,34 @@
 
 // 借助于模板的模板参数，可以只指定容器的类型而不需要指定所含元素的类型 就像这样:
 // Stack<int, std::vector> vStack;
+// 为了获得这个特性，必须把第2个模板参数指定为模板的模板参数
 
+// 注： 函数模板并不支持模板的模板参数 (template template argument)
+
+#include <deque>
+#include <vector>
+#include <iostream>
+
+template <typename T, template <typename ELEM, typename ALLOC = std::allocator<ELEM> > class CONT = std::deque >
+//template <typename T, template <typename ELEM, typename ALLOC = std::allocator<ELEM> > typename CONT = std::deque >
+class Stack {
+private:
+    CONT<T> rep_;
+public:
+    void push(T const& e) {
+        rep_.push_back(e);
+    }
+
+    T top() const {
+        if (rep_.empty()) {
+            throw std::out_of_range("Stack<>::top() stack is empty");
+        }
+        return rep_.back();
+    }
+};
 
 int main(void) {
-
+    Stack<int, std::vector> s;
+    s.push(1);
+    std::cout << s.top() << std::endl;
 }
-
-
